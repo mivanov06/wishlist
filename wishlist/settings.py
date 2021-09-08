d4faa17e4488c4c23c649ea261585f8acdce3894
mivanov <mivanov@rtrn.ru>
2021-09-08 11:59:11 +1000

    added page About

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*&&iv_aqg+z9d@ox_%6u8k7jc!!x&l7f+707$5i3am78$qa^i'
+SECRET_KEY = 'django-insecure-!7eytjl61)%@-loh=*8#+si#i)s25^q_4ekzr@haiu4zo%4npm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [

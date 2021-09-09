903b376073bacdacda9a2397593344bc387dee97
mivanov06 <mivanov06@yandex.ru>
2021-09-09 23:55:28 +1000

    added model WishList

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     """
@@ -13,6 +13,16 @@ class Product(models.Model):
     link = models.URLField()
     price = models.IntegerField()
     create_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.title
+
+
+class WishList(models.Model):
+    title = models.CharField(max_length=120)
+    product = models.ManyToManyField(Product)
+    is_hidden = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title

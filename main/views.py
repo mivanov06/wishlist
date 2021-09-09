c1f85fee5385ae86e9ebe3e55556020fd73195af
mivanov06 <mivanov06@yandex.ru>
2021-09-10 00:36:09 +1000

    added wish_list.html added list_page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import WishList
 
 
 def index(request):
@@ -7,3 +9,9 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html', {"title": "Wishlist | about project"})
+
+
+def list_page(request, pk):
+    wishlist = get_object_or_404(WishList, pk=pk)
+    print('[wishlist]', wishlist)
+    return render(request, 'wish_list.html', {'list': wishlist})
